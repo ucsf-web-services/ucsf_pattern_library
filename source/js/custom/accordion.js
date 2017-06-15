310c420606ae1453ace3a6d710ce687a80e47a01
K2 <kristin.bradham@gmail.com>
2017-06-15 11:10:18 -0700

    Accordions update

--- source/js/custom/accordion.js
+++ source/js/custom/accordion.js
@@ -1,29 +1,12 @@
 //Accordion 1
 
-var acc = document.getElementsByClassName("accordion");
+var acc = document.getElementsByClassName("accordion__header");
 var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].onclick = function(){
     this.classList.toggle("active");
     this.classList.toggle("downarrow");
-    var accordion__header = this.nextElementSibling;
-    if (accordion__header.style.display === "block") {
-      accordion__header.style.display = "none";
-    } else {
-      accordion__header.style.display = "block";
-    }
-  }
-}
-
-//Accordion 2
-var acc = document.getElementsByClassName("accordion-2");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].onclick = function(){
-    this.classList.toggle("active");
-    this.classList.toggle("expand");
     var accordion__header = this.nextElementSibling;
     if (accordion__header.style.display === "block") {
       accordion__header.style.display = "none";
