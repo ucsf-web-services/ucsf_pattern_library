5861cae5e3a33c0e79c6af3835a9cd4e704e1510
Chris Darke <chrisdarke42@gmail.com>
2017-06-22 17:04:19 -0600

    Fixing responsive hero-1

--- source/js/custom/slideshow.js
+++ source/js/custom/slideshow.js
@@ -2,4 +2,16 @@ $(document).ready(function(){
   $('.slider').slick({
     // Settings
   });
+
+  $('.slider-hero').slick({
+    slide: '.hero',
+    variableWidth: false,
+    slidesToShow: 1,
+    infinite: true,
+    centerMode: false,
+    arrows: true,
+    dots: false,
+    autoplay: false
+  });
+
 });
