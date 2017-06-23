7afc82f693c2671131cd7c87bd23cb0916e9771b
Chris Darke <chrisdarke42@gmail.com>
2017-06-22 22:15:53 -0600

    Adding slide counter

--- source/js/custom/slideshow.js
+++ source/js/custom/slideshow.js
@@ -3,7 +3,18 @@ $(document).ready(function(){
     // Settings
   });
 
-  $('.slider-hero').slick({
+  var $slickElement = $('.slider-hero');
+  $('.hero').append("<div class='paging-info'></div>");
+  var $status = $('.paging-info');
+
+  $slickElement.on('init reInit afterChange', function(event, slick, currentSlide, nextSlide){
+    //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+    var i = (currentSlide ? currentSlide : 0) + 1;
+    var total = $('.hero:not(.slick-cloned)').length;
+    $status.text(i + ' of ' + total);
+  });
+
+  $slickElement.slick({
     slide: '.hero',
     variableWidth: false,
     slidesToShow: 1,
@@ -13,5 +24,6 @@ $(document).ready(function(){
     dots: false,
     autoplay: false
   });
+
 
 });
