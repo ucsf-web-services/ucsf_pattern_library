b10f358ae6baca637e2a8ec0ade78765d815380d
Chris Darke <chrisdarke42@gmail.com>
2017-06-30 16:30:27 -0600

    Adding Hero 3 responsive

--- source/js/custom/slideshow.js
+++ source/js/custom/slideshow.js
@@ -11,6 +11,9 @@ $(document).ready(function(){
   // On Hero 2 append pager to the inner text wrapper
   $('.hero-2__text-wrapper').append("<div class='paging-info'></div>");
 
+  // On Hero 3 append pager to the main slider
+  $('.hero-3').append("<div class='paging-info'></div>");
+
   var $status = $('.paging-info');
 
   $slickElement.on('init reInit afterChange', function(event, slick, currentSlide, nextSlide){
